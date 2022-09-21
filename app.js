4e4d069956b0ec679565573dcd84527dbc5b4e95
fsdmern <fsd.mern@gmail.com>
2022-09-21 07:33:47 +0530

    css units covered

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 $(document).ready(function () {
-  $('#screen').load('abs.html');
+  $('#screen').load('1.abs.html');
 
-  $(document).on('click', '#rem_button', function (event) {
-    $('#screen').load('rem.html');
+  $(document).on('click', '#abs_button', function (event) {
+    $('#screen').load('1.abs.html');
+  });
+
+  $(document).on('click', '#per_button', function (event) {
+    $('#screen').load('2a.percent.html');
   });
 
   $(document).on('click', '#em_button', function (event) {
-    $('#screen').load('em.html');
+    $('#screen').load('2b.em.html');
+  });
+
+  $(document).on('click', '#rem_button', function (event) {
+    $('#screen').load('2c.rem.html');
   });
 
   $(document).on('click', '#vwvh_button', function (event) {
-    $('#screen').load('vwvh.html');
+    $('#screen').load('2d.vwvh.html');
   });
-
-  $(document).on('click', '#abs_button', function (event) {
-    $('#screen').load('abs.html');
-  });
-  
 });
